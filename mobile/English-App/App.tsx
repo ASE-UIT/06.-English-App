bdd208e5f21b07e6161978fc9a824d5092aeeeb2
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-09-30 21:54:26 +0700

    native wind setup

--- mobile/English-App/App.tsx
+++ mobile/English-App/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/screens/Home";
+import Test from "./src/screens/TestScreen/Test";
 
 export default function App() {
   return (
-    <View>
-      <Home />
+    <View className=" bg-black">
+
     </View>
   );
 }
---
+++ mobile/English-App/src/screens/TestScreen/Test.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Test = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+export default Test;
