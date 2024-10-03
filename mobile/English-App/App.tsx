9453f5edb25ad093073f195d8fead9e44a998077
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-04 00:39:11 +0700

    feature: sentence_complete writing_sumit without style

--- mobile/English-App/App.tsx
+++ mobile/English-App/App.tsx
@@ -2,11 +2,34 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/screens/Home";
 import Test from "./src/screens/TestScreen/Test";
+import Sentence_completion_format from "./src/components/Sentence_completion_format";
+import Writing_submit from "./src/components/Writing_submit";
+
+const questionList = [
+  {
+    question: "I ___ a cat. I love ___ fish",
+  },
+  {
+    question: "I ___ a cat. I love ___ fish",
+    
+  },
+  {
+    question: "I ___ a cat. I love ___ fish",
+    
+  },
+  {
+    question: "I ___ a cat. I love ___ fish",
+  },
+  {
+    question: "I ___ a cat. I love ___ fish",
+  },
+]
 
 export default function App() {
   return (
-    <View className=" bg-black">
-
+    <View className="items-center m-10">
+        <Sentence_completion_format questionList={questionList} questionNumber={1} />
+        <Writing_submit />
     </View>
   );
 }
