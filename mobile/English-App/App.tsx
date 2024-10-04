26f90ef12f20818e12197b3e82a2397284640cc1
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-04 14:34:54 +0700

    merge before push

--- mobile/English-App/App.tsx
+++ mobile/English-App/App.tsx
@@ -5,31 +5,12 @@ import Test from "./src/screens/TestScreen/Test";
 import Sentence_completion_format from "./src/components/Sentence_completion_format";
 import Writing_submit from "./src/components/Writing_submit";
 
-const questionList = [
-  {
-    question: "I ___ a cat. I love ___ fish",
-  },
-  {
-    question: "I ___ a cat. I love ___ fish",
-    
-  },
-  {
-    question: "I ___ a cat. I love ___ fish",
-    
-  },
-  {
-    question: "I ___ a cat. I love ___ fish",
-  },
-  {
-    question: "I ___ a cat. I love ___ fish",
-  },
-]
+
 
 export default function App() {
   return (
     <View className="items-center m-10">
-        <Sentence_completion_format questionList={questionList} questionNumber={1} />
-        <Writing_submit />
+        
     </View>
   );
 }
