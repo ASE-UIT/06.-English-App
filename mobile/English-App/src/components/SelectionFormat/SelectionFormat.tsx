f220646ea53d80088c7f248f368273a713159325
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-04 22:35:50 +0700

    feat: update selection format

--- mobile/English-App/src/components/SelectionFormat/SelectionFormat.tsx
+++ mobile/English-App/src/components/SelectionFormat/SelectionFormat.tsx
@@ -4,11 +4,13 @@ import QuestionHeading from '../QuestionHeading'
 import SelectionQuestion from './SelectionQuestion'
 
 const data = [
-    'A',
-    'B',
-    'C',
-    'D',
-    ]
+  { id: 1, question: 'What is the capital of France?' },
+  { id: 2, question: 'Which planet is known as the Red Planet?' },
+  { id: 3, question: 'Which ocean is the largest?' },
+  { id: 4, question: 'Who wrote "Romeo and Juliet"?' },
+  { id: 5, question: 'What is the chemical formula for water?' }
+]
+const options = ['A', 'B', 'C', 'D'];
 
 
 
@@ -18,7 +20,11 @@ const SelectionFormat = () => {
       <QuestionHeading from={1} to={5} />
       <Text className="sub-heading text-black text-lg">Choose the correct letter A, B, C or D.</Text>
       <Text className="sub-heading text-black text-lg">Write the correct letter in boxes 1-5 on your answer sheet</Text>
-      <SelectionQuestion number={1} items={data}  />
+      <View className="questions-container">
+        {data.map((question) => (
+          <SelectionQuestion key={question.id} id={question.id} question={question.question} options={options} />
+        ))}
+      </View>
 
     </View>
   )
--- mobile/English-App/src/components/SelectionFormat/SelectionQuestion.tsx
+++ mobile/English-App/src/components/SelectionFormat/SelectionQuestion.tsx
@@ -1,25 +1,38 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { List } from "react-native-paper";
+import { ListItem } from "@rneui/themed";
+import { ListItemAccordion } from "@rneui/base/dist/ListItem/ListItem.Accordion";
+
 type SelectionFormatProps = {
-  number: number;
-  items: string[];
+  id: number;
+  question: string;
+  options: string[];
 };
 
-const SelectionQuestion = ({ number, items }: SelectionFormatProps) => {
+const SelectionQuestion = ({ id, question, options }: SelectionFormatProps) => {
+  const [expanded, setExpanded] = React.useState(false);
   return (
     <View>
-      <Text className=" text-lg">{number}.</Text>
-      <List.Section>
-        {items.map((item, index) => (
-          <List.Item 
-          key={index} 
-          title={item} 
-            />
+      <Text className="text-lg">{id}.</Text>
+      <ListItemAccordion
+        isExpanded={expanded}
+        onPress={() => setExpanded(!expanded)}
+        noIcon
+        noRotation
+        bottomDivider
+        className=" flex-shrink"
+      >
+        {options.map((option, i) => (
+          <ListItem key={i}>
+            <ListItem.Content>
+              <ListItem.Title>{option}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
         ))}
-      </List.Section>
+      </ListItemAccordion>
+
+      <Text className="text-lg">{question}</Text>
     </View>
   );
 };
-
 export default SelectionQuestion;
