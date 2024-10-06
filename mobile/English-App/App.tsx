d6a5ffd171e6bd7dd7917ed55ed8ad329466e515
Tri Duong <duongthuantri159@gmail.com>
2024-10-06 19:44:28 +0700

    Feat: Implement the Video Learning feature - prototype

--- mobile/English-App/App.tsx
+++ mobile/English-App/App.tsx
@@ -1,11 +1,12 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 import MultipleChoiceFormat from "./src/components/SelectionQuestion/MultipleChoiceFormat";
+import VideoLearning from "./src/components/VideoLearning/VideoLearning";
 
 export default function App() {
   return (
     <SafeAreaView className="w-full h-full pl-4 pt-16">
-      {/* <MultipleChoiceFormat /> */}
+      <VideoLearning />
     </SafeAreaView>
   );
 }
