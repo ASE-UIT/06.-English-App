ca2e4d191d3aa8f03d4d822cb789e06c7edc2a90
Tri Duong <duongthuantri159@gmail.com>
2024-10-10 12:06:34 +0700

    Refactor: Change some styles related to MultiplechoiceFormat

--- mobile/English-App/src/components/QuestionSubHeading.tsx
+++ mobile/English-App/src/components/QuestionSubHeading.tsx
@@ -1,16 +1,5 @@
 import { Text } from "react-native";
 
-export default function QuestionSubHeading({
-  from,
-  to,
-}: {
-  from: number;
-  to: number;
-}) {
-  return (
-    <Text className="sub-heading text-black text-lg">
-      Choose the correct letter A,B,C or D. {"\n"}Write the correct letter in
-      boxes {from}-{to} on your answer sheet.
-    </Text>
-  );
+export default function QuestionSubHeading({ text }: { text: string }) {
+  return <Text className="sub-heading text-[#7879F1] text-base">{text}</Text>;
 }
