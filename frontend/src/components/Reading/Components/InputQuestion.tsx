25be728ff682c171e67778cf8798e9168252a1fd
Lmt310104 <lmtoan311@gmail.com>
2024-10-10 13:53:10 +0700

    feat: reading/true-false-notgiven

--- frontend/src/components/Reading/Components/InputQuestion.tsx
+++ frontend/src/components/Reading/Components/InputQuestion.tsx
@@ -4,9 +4,9 @@ import { PlusCircledIcon } from "@radix-ui/react-icons"
 
 
 
-const InputQuestion = () => { 
+const InputQuestion = ({className}: {className: string}) => { 
     return (
-        <div className="flex">
+        <div className={`flex ${className}`}>
             <Input className="bg-customPink text-[#AEAEB2] px-[12px] py-[16px] " placeholder="Type here"/>
             <Button variant="outline" size="icon" className="bg-inherit flex items-center justify-center p-0"> <PlusCircledIcon className="h-5 w-5 bg-inherit text-[black]" /></Button>
         </div>
