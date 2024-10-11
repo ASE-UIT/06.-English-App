b843ef1a073f1a333bd179e20e33d3523544c485
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-11 22:08:12 +0700

    feat: raw progress bar

--- mobile/English-App/src/components/ProgressBar/ProgressBar.tsx
+++ mobile/English-App/src/components/ProgressBar/ProgressBar.tsx
@@ -1,16 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CircleContainer from './CircleContainer'
-import { Icon } from 'react-native-elements'
-
+import { View, Text } from "react-native";
+import React from "react";
+import CircleContainer from "./CircleContainer";
+import { Icon } from "react-native-elements";
 
 const ProgressBar = () => {
+  const data = [1, 2, 3, 4, 5];
   return (
-    <View className=' flex flex-row h-[48px] w-full bg-secondaryExtraLight items-center justify-center'>
-      
+    <View className=" flex flex-row h-[48px] w-full bg-secondaryExtraLight items-center justify-between">
+      <Icon name="arrow-back-ios" type="material" size={32} color={"#EF5DA8"} className="ml-1" />
+      <View className=" flex flex-row justify-items-center items-center ">
+      {data.map((index) => (
+        <View className=" mx-1">
+          <CircleContainer index={index} />
+        </View>
+      ))}
+      </View>
+      <Icon name="arrow-forward-ios" type="material" size={32} color={"#EF5DA8"} className=" " />
     </View>
-  )
-}
+  );
+};
 
-
-export default ProgressBar
+export default ProgressBar;
