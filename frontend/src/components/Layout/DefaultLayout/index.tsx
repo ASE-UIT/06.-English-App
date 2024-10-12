dbe36ae57c4ff46cdeb617be4443949b089a1606
Lmt310104 <lmtoan311@gmail.com>
2024-10-12 14:02:48 +0700

    merge main

--- frontend/src/components/Layout/DefaultLayout/index.tsx
+++ frontend/src/components/Layout/DefaultLayout/index.tsx
@@ -3,8 +3,6 @@ import Header from "@/components/Tam-components/Header"
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-white">
-      <Header />
-      <div className="mb-4 w-full flex-1">{children}</div>
       <Header></Header>
       <div className="mb-4 max-w-screen-2xl flex-1">{children}</div>
     </div>
