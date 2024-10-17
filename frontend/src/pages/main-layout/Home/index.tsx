88596ce810a156ddc6dfca1b695ecd733ac92f24
Lmt310104 <lmtoan311@gmail.com>
2024-10-18 06:30:53 +0700

    fix: header in teacher's view

--- frontend/src/pages/main-layout/Home/index.tsx
+++ frontend/src/pages/main-layout/Home/index.tsx
@@ -1,9 +1,6 @@
-import Header from "@/components/Tam-components/Header"
-
 export const Home = () => {
   return (
     <>
-      <Header />
       <div className="w-screen bg-red-400">Hello</div>
     </>
   )
