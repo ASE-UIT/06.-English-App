0d4a43fc280d4df657be7c8cea10373eb2abab90
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-20 16:06:16 +0700

    feat: codebase writing

--- mobile/English-App/App.tsx
+++ mobile/English-App/App.tsx
@@ -1,5 +1,8 @@
 import { SafeAreaView } from "react-native";
+import WritingExercise from "./src/screens/WritingExercise";
 
 export default function App() {
-  return <SafeAreaView className="w-full h-full  pt-16"></SafeAreaView>;
+  return <SafeAreaView className="w-full h-full  pt-16">
+    <WritingExercise />
+  </SafeAreaView>;
 }
---
+++ mobile/English-App/src/screens/WritingExercise.tsx
@@ -0,0 +1,57 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Button } from '@rneui/themed'
+import Writing_task from '../components/Writing/Writing_task'
+import Writing_submit from '../components/Writing/Writing_submit'
+
+const WritingExercise = () => {
+    const [task, setTask] = useState(true);
+
+    const handleTask = () => {
+        setTask(true); // Set to "true" for the "Task" button
+    };
+
+    const handleWriting = () => {
+        setTask(false); // Set to "false" for the "Writing" button
+    };
+    
+    const [textInputValue, setTextInputValue] = useState('');
+    return (
+        <View>
+            {
+                task ? (
+                    <Writing_task taskNumber={0} question={''} />
+                ) : (
+                    <Writing_submit />
+                )
+            }
+
+            <View className='flex flex-row justify-between p-3 gap-2'>
+                <Button
+                    title={'Task'}
+                    containerStyle={{
+                    }}
+                    onPress={handleTask}
+                    buttonStyle={{ 
+                        backgroundColor: '#EF5DA8',
+                        borderRadius: 12,
+                        width: 100,
+                    }}
+                />
+                <Button
+                    title={'Writing'}
+                    containerStyle={{
+                    }}
+                    onPress={handleWriting}
+                    buttonStyle={{ 
+                        backgroundColor: '#EF5DA8',
+                        borderRadius: 12,
+                        width: 100,
+                    }}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default WritingExercise
