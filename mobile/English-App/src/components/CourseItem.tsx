e8c0f3629762f836c1cd3c863f25d7d12e550c78
Nguyen Trinh Vinh Thinh <vinhthinhvippro@gmail.com>
2024-11-03 18:42:48 +0700

    Style: make font bigger

--- mobile/English-App/src/components/CourseItem.tsx
+++ mobile/English-App/src/components/CourseItem.tsx
@@ -10,14 +10,14 @@ export default function CourseItem({ srcImg, title, teacherName, progress, rated
             <Image className='mt-1 aspect-square max-w-[50px] w-full max-h-[50px] h-full object-cover object-center' src={srcImg} />
         </View>
         <View className='w-[78%] flex flex-col'>
-            <View className='flex-row items-center justify-between mb-[4px]'>
-                <Text className='text-sm font-bold w-[90%]'>{title}</Text>
+            <View className='flex-row items-center justify-between mb-[2px]'>
+                <Text className='text-[16px] font-bold w-[90%]'>{title}</Text>
                 <Icon className='' color={'#5D5FEF'} name='more-vert' type='material' size={15} onPress={() => {}}/>
             </View>
-            <Text className='block font-normal text-xs text-[#7879F1] mb-[6px]'>{teacherName}</Text>
+            <Text className='block font-normal text-[13px] text-[#7879F1] mb-[5px]'>{teacherName}</Text>
             <LinearProgress value={progress/100} variant="determinate" className='w-full mb-[6px]' color='#EF5DA8' />
             <View className='flex flex-row items-center justify-between mb-[6px]'>
-                <Text  className='text-[8px] font-normal text-[#A49E9E]'>{progress}% complete</Text>
+                <Text  className='text-[10px] font-normal text-[#A49E9E]'>{progress}% complete</Text>
                 <View className='flex flex-row items-center gap-[2px]'>
                     {[...Array(rated)].map((_, i) => (
                         <Icon key={i} name='star' type='material' size={10} color='#FFD700' />
