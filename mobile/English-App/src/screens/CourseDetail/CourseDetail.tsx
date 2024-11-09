11923d450e154360a4e514050c0fc8fc78ed15e6
Tri Duong <duongthuantri159@gmail.com>
2024-11-09 23:48:10 +0700

    Feat: Payment screens

--- mobile/English-App/src/screens/CourseDetail/CourseDetail.tsx
+++ mobile/English-App/src/screens/CourseDetail/CourseDetail.tsx
@@ -1,100 +1,240 @@
-import { View, Text, Image, FlatList } from 'react-native'
-import React from 'react'
-import { Button, Icon } from '@rneui/themed'
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
+import { Button, Icon } from "@rneui/themed";
+import { BottomSheet } from "@rneui/base";
 
 const Lesson = () => {
   return (
-    <View className='flex gap-1 mt-[10px]'>
-      <Text className='text-base font-semibold'>Lesson 1</Text>
-      <Text style={{ color: 'rgba(0, 0, 0, 0.7)' }} className='text-sm font-medium'>Introduction</Text>
+    <View className="flex gap-1 mt-[10px]">
+      <Text className="text-base font-semibold">Lesson 1</Text>
+      <Text
+        style={{ color: "rgba(0, 0, 0, 0.7)" }}
+        className="text-sm font-medium"
+      >
+        Introduction
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 const CourseDetail = () => {
-
-  const [isPlaylist, setIsPlaylist] = React.useState(true)
-  const [isDescription, setIsDescription] = React.useState(false)
+  const [isPlaylist, setIsPlaylist] = React.useState(true);
+  const [isDescription, setIsDescription] = React.useState(false);
 
   const handlePlaylist = () => {
-    setIsPlaylist(true)
-    setIsDescription(false)
-  }
+    setIsPlaylist(true);
+    setIsDescription(false);
+  };
   const handleDescription = () => {
-    setIsPlaylist(false)
-    setIsDescription(true)
-  }
+    setIsPlaylist(false);
+    setIsDescription(true);
+  };
 
-
+  const [isBottomSheetVisible, setIsBottomSheetVisible] = React.useState(false);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = React.useState(0);
   return (
-    <View className='relative w-full h-full'>
-      <View className='flex mx-[45px] mt-[50px] '>
-        <Image source={require('../../../assets/Frame9.png')} className='rounded-3xl w-full h-64'/>
-        <Text className='text-lg font-semibold'>Reading Course For Beginer</Text>
-        <Text>Created by <Text className='text-[#5D5FEF]'>Ms. Thuy</Text></Text>
-        <View className='flex flex-row justify-between'>
-          <View className='flex flex-row items-center gap-1'>
-            <Icon name='star-o' type='font-awesome' onPress={() => {console.log('Press')}}/>
+    <View className="relative w-full h-full">
+      <View className="flex mx-[45px] mt-[50px] ">
+        <Image
+          source={require("../../../assets/Frame9.png")}
+          className="rounded-3xl w-full h-64"
+        />
+        <Text className="text-lg font-semibold">
+          Reading Course For Beginer
+        </Text>
+        <Text>
+          Created by <Text className="text-[#5D5FEF]">Ms. Thuy</Text>
+        </Text>
+        <View className="flex flex-row justify-between">
+          <View className="flex flex-row items-center gap-1">
+            <Icon
+              name="star-o"
+              type="font-awesome"
+              onPress={() => {
+                console.log("Press");
+              }}
+            />
             <Text>4.5</Text>
           </View>
-          <Text className='text-[#5D5FEF] text-3xl'>40$</Text>
+          <Text className="text-[#5D5FEF] text-3xl">40$</Text>
         </View>
 
-        <View >
-          <View className='flex flex-row py-3 bg-[#A5A6F6] justify-around rounded-[20px]'>
-            <Button title='Playlist' buttonStyle={{
-              backgroundColor: isPlaylist ? '#5D5FEF' : '#A5A6F6',
-              borderRadius: 20,
-              width: 120,
-              height: 50,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-            onPress={handlePlaylist}
+        <View>
+          <View className="flex flex-row py-3 bg-[#A5A6F6] justify-around rounded-[20px]">
+            <Button
+              title="Playlist"
+              buttonStyle={{
+                backgroundColor: isPlaylist ? "#5D5FEF" : "#A5A6F6",
+                borderRadius: 20,
+                width: 120,
+                height: 50,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={handlePlaylist}
             />
-            <Button title='Description' buttonStyle={{
-              backgroundColor: !isDescription ? '#A5A6F6' : '#5D5FEF',
-              borderRadius: 20,
-              width: 120,
-              height: 50,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-            onPress={handleDescription}
+            <Button
+              title="Description"
+              buttonStyle={{
+                backgroundColor: !isDescription ? "#A5A6F6" : "#5D5FEF",
+                borderRadius: 20,
+                width: 120,
+                height: 50,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={handleDescription}
             />
           </View>
-          <FlatList 
-            data={['Lesson 1', 'Lesson 2', 'Lesson 3', 'Lesson 4', 'Lesson 5', 'Lesson 6', 'Lesson 7', 'Lesson 8', 'Lesson 9', 'Lesson 10']} 
-            renderItem={({ item }) => Lesson()} 
-            style={{ display:'flex',height: '50%', marginTop: 10}}
-            scrollEnabled={true}
-            keyExtractor={(item, index) => index.toString()} 
-          />
         </View>
-        
       </View>
-      <View className='flex flex-row bg-white pl-8 pr-8 pb-3 justify-around absolute bottom-0 right-0 left-0'>
-        <Button buttonStyle={{
-          backgroundColor: '#EF5DA8',
-          borderRadius: 20,
-          width: 60,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-            <Icon name='cart-outline' type='material-community' onPress={() => {console.log('Press')}}/>
+      <View className="flex flex-row bg-white pl-8 pr-8 pb-3 justify-around">
+        <Button
+          buttonStyle={{
+            backgroundColor: "#EF5DA8",
+            borderRadius: 20,
+            width: 60,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Icon
+            name="cart-outline"
+            type="material-community"
+            onPress={() => {
+              console.log("Press");
+            }}
+          />
         </Button>
-        <Button title='BUY NOW' buttonStyle={{
-          backgroundColor: '#5D5FEF',
-          borderRadius: 20,
-          width: 210,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}/>
+        <Button
+          title="BUY NOW"
+          onPress={() => {
+            setIsBottomSheetVisible(true);
+          }}
+          buttonStyle={{
+            backgroundColor: "#5D5FEF",
+            borderRadius: 20,
+            width: 210,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
       </View>
+      <BottomSheet
+        isVisible={isBottomSheetVisible}
+        modalProps={{
+          animationType: "slide",
+        }}
+      >
+        <View className="flex flex-col items-center justify-between pb-4 h-[50vh] bg-white rounded-2xl">
+          <View className="header flex flex-row justify-between w-full p-4">
+            <Text className="text-lg text-gray-500">Choose payment method</Text>
+            <Icon
+              name="close"
+              type="material-community"
+              onPress={() => {
+                setIsBottomSheetVisible(false);
+              }}
+            />
+          </View>
+          <View className="payment-method-cards flex flex-col gap-2 items-center w-3/4">
+            <TouchableOpacity
+              className="payment-method-card"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 16,
+                width: "100%",
+                padding: 15,
+                borderRadius: 20,
+                borderStyle: "solid",
+                borderWidth: selectedPaymentMethod === 0 ? 1 : 0,
+                borderColor: "#6b7280",
+                backgroundColor:
+                  selectedPaymentMethod !== 0
+                    ? "rgba(165, 166, 246, 0.2)"
+                    : "transparent",
+                position: "relative",
+              }}
+              onPress={() => {
+                setSelectedPaymentMethod(0);
+              }}
+            >
+              <Icon
+                name="credit-card"
+                type="font-awesome"
+                color="#5D5FEF"
+                size={40}
+              />
+              <Text className="text-lg">Pay with card</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="payment-method-card"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 16,
+                width: "100%",
+                padding: 15,
+                borderRadius: 20,
+                borderStyle: "solid",
+                borderWidth: selectedPaymentMethod === 1 ? 1 : 0,
+                borderColor: "#6b7280",
+                backgroundColor:
+                  selectedPaymentMethod !== 1
+                    ? "rgba(165, 166, 246, 0.2)"
+                    : "transparent",
+                position: "relative",
+              }}
+              onPress={() => {
+                setSelectedPaymentMethod(1);
+              }}
+            >
+              <Icon name="bank" type="font-awesome" color="#5D5FEF" size={40} />
+              <Text className="text-lg">Pay with bank</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            className="security-standards-info"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-around",
+              gap: 8,
+              backgroundColor: "#E3F7EF",
+              borderRadius: 15,
+              width: "85%",
+              padding: 10,
+            }}
+          >
+            <Icon name="check" type="font-awesome" color="#10B981" size={10} />
+            <Text className="text-xs text-gray-500">
+              We adhere entirely to the data security standards of the payment
+              card industry.
+            </Text>
+          </View>
+          <View className="button-container w-full flex items-center">
+            <Button
+              title="Continue"
+              buttonStyle={{
+                backgroundColor: "#5D5FEF",
+                borderRadius: 40,
+                width: 210,
+                height: 50,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            />
+          </View>
+        </View>
+      </BottomSheet>
     </View>
-  )
-}
+  );
+};
 
-export default CourseDetail
+export default CourseDetail;
---
+++ mobile/English-App/src/screens/CourseDetail/PayWithBank.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { Text, View, TextInput } from "react-native";
+import { Button } from "@rneui/themed";
+import { Dropdown } from "react-native-element-dropdown";
+
+export default function PayWithBank() {
+  return (
+    <View className="w-full h-full">
+      <View className="h-full mt-[50px] mx-[16px] flex flex-col justify-around items-center">
+        <Text className="text-[64px] text-blue1">$40</Text>
+        <View className="form w-full flex gap-4">
+          <View className="flex flex-col flex-wrap gap-1">
+            <Text className="text-lg font-semibold">Card Information</Text>
+            <TextInput
+              placeholder="Card Number"
+              style={{
+                width: "100%",
+                borderWidth: 1,
+                borderColor: "#5D5FEF",
+                borderRadius: 5,
+                padding: 5,
+                fontSize: 20,
+              }}
+            />
+          </View>
+          <View className="flex flex-col flex-wrap gap-1">
+            <Text className="text-lg font-semibold">Bank</Text>
+            <Dropdown
+              data={[
+                { label: "Vietcombank" },
+                { label: "Techcombank" },
+                { label: "Vietinbank" },
+              ]}
+              labelField={"label"}
+              valueField={"label"}
+              value="Vietcombank"
+              onChange={(value) => console.log(value)}
+              style={{
+                width: "100%",
+                borderWidth: 1,
+                borderColor: "#5D5FEF",
+                borderRadius: 5,
+                padding: 5,
+              }}
+              selectedTextStyle={{
+                fontSize: 20,
+              }}
+            />
+          </View>
+          <View className="flex flex-col flex-wrap gap-1">
+            <Text className="text-lg font-semibold">Country or region</Text>
+            <Dropdown
+              data={[
+                {
+                  label: "Vietnam",
+                  value: "Vietnam",
+                },
+                {
+                  label: "USA",
+
+                  value: "USA",
+                },
+              ]}
+              labelField={"label"}
+              valueField={"value"}
+              onChange={(value) => console.log(value)}
+              style={{
+                width: "100%",
+                borderWidth: 1,
+                borderColor: "#5D5FEF",
+                borderRadius: 5,
+                padding: 5,
+              }}
+              selectedTextStyle={{
+                fontSize: 20,
+              }}
+              value={"Vietnam"}
+            />
+          </View>
+          <View className="flex flex-col flex-wrap gap-1">
+            <Text className="text-lg font-semibold">OTP</Text>
+            <TextInput
+              placeholder="6-digit code"
+              style={{
+                width: "100%",
+                borderWidth: 1,
+                borderColor: "#5D5FEF",
+                borderRadius: 5,
+                padding: 5,
+                fontSize: 20,
+              }}
+            />
+          </View>
+        </View>
+        <Button
+          title="Pay"
+          buttonStyle={{
+            borderRadius: 30,
+            width: 120,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#5D5FEF",
+          }}
+          titleProps={{
+            style: {
+              fontSize: 24,
+              color: "white",
+            },
+          }}
+        />
+      </View>
+    </View>
+  );
+}
