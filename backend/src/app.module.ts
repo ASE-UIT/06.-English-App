962d9541b9f1eca761995072e461ad29ce8762a3
Vn-Boy <0939899928za.com@gmail.com>
2024-11-12 23:52:49 +0700

    feat: update redis service missing

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { AutomapperModule } from '@automapper/nestjs';
 import { classes } from '@automapper/classes';
 import { SharedModule } from './module/shared.module';
 import { minutes, ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -24,6 +26,12 @@ import { minutes, ThrottlerModule } from '@nestjs/throttler';
         limit: 15,
       },
     ]),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
     SharedModule,
   ],
 })
