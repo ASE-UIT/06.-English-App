6f09ea5c94cd424601c4acc16b50cc443c52a66c
Vn-Boy <0939899928za.com@gmail.com>
2024-11-13 11:46:45 +0700

    fix: final2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import configuration from './config/configuration';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from './datasource/typeorm.module';
 import { AutomapperModule } from '@automapper/nestjs';
 import { classes } from '@automapper/classes';
 import { SharedModule } from './module/shared.module';
 import { minutes, ThrottlerModule } from '@nestjs/throttler';
-import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -26,16 +24,6 @@ import * as redisStore from 'cache-manager-redis-store';
         limit: 15,
       },
     ]),
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        store: redisStore,
-        host: configService.get<string>('redis.host'),
-        port: configService.get<number>('redis.port'),
-      }),
-    }),
     SharedModule,
   ],
 })
