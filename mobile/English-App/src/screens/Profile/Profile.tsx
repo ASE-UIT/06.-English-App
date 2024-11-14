7266ac3365dc97682982eb1b90cbd2a4d23ae75c
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2024-11-14 21:37:06 +0700

    feat: fetch user api

--- mobile/English-App/src/screens/Profile/Profile.tsx
+++ mobile/English-App/src/screens/Profile/Profile.tsx
@@ -1,16 +1,44 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, TextInput, TouchableWithoutFeedback, TouchableOpacity, Keyboard } from 'react-native';
-import { Avatar, Icon } from 'react-native-elements';
-import InputField from './InputField'; // Adjust the import path as needed
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
+import { Avatar, Icon } from "react-native-elements";
+import InputField from "./InputField"; // Adjust the import path as needed
+import userService from "../../services/user.service";
 
 const Profile = () => {
-  const [phoneNumber, setPhoneNumber] = useState('1234567890');
-  const [email, setEmail] = useState('exam@gmail.com');
-  const [password, setPassword] = useState('*******');
-  const [username, setUsername] = useState('johndoe');
-  const [name, setName] = useState('John Doe');
+  const [phoneNumber, setPhoneNumber] = useState("1234567890");
+  const [email, setEmail] = useState("exam@gmail.com");
+  const [password, setPassword] = useState("*******");
+  const [username, setUsername] = useState("johndoe");
+  const [name, setName] = useState("John Doe");
   const [isEditingName, setIsEditingName] = useState(false);
   const nameInputRef = useRef<TextInput>(null);
+
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await userService.getUser();
+        if (result.statusCode === 200) {
+          const userData = result.data;
+          setUsername(`${userData.firstName} ${userData.lastName}`);
+          setPhoneNumber(userData.phone);
+          setEmail(userData.email);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+    console.log(data);
+  }, []);
 
   const handleEditName = () => {
     setIsEditingName(!isEditingName);
@@ -25,11 +53,11 @@ const Profile = () => {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View className="flex flex-col justify-center items-center">
         <Avatar
-          source={{ uri: 'https://randomuser.me/api/portraits/men/36.jpg' }}
+          source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
           size="xlarge"
           rounded
           containerStyle={{ marginTop: 20 }}
-          onPress={() => console.log('Works!')}
+          onPress={() => console.log("Works!")}
         />
         <View className="mt-5 flex flex-row items-center">
           {isEditingName ? (
@@ -39,7 +67,7 @@ const Profile = () => {
               onChangeText={setName}
               onBlur={handleEditName}
               autoFocus
-              style={{ fontSize: 24, color: '#5d5fef', fontWeight: 'bold' }}
+              style={{ fontSize: 24, color: "#5d5fef", fontWeight: "bold" }}
             />
           ) : (
             <Text className="text-2xl text-primary font-black mr-1">
@@ -55,44 +83,61 @@ const Profile = () => {
               borderRadius: 999,
               width: 30,
               height: 30,
-              justifyContent: 'center',
-              alignItems: 'center',
+              justifyContent: "center",
+              alignItems: "center",
             }}
           />
         </View>
-        <InputField label="Username" value={username} onChangeText={setUsername} />
-        <InputField label="Phone Number" value={phoneNumber} onChangeText={setPhoneNumber} />
+        <InputField
+          label="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <InputField
+          label="Phone Number"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
         <InputField label="Email" value={email} onChangeText={setEmail} />
-        <InputField label="Password" value={password} onChangeText={setPassword} />
+        <InputField
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
         <View
-        style={{ marginTop: 100 }}
-        className="flex flex-row justify-center items-center  gap-7"
-      >
-        <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
-          <Icon
-            name="checkbox-marked-outline"
-            type="material-community"
-            color="white"
-            size={25}
-          />
-          <Text className="text-white text-xs  font-semibold">Update</Text>
-        </TouchableOpacity>
+          style={{ marginTop: 100 }}
+          className="flex flex-row justify-center items-center  gap-7"
+        >
+          <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
+            <Icon
+              name="checkbox-marked-outline"
+              type="material-community"
+              color="white"
+              size={25}
+            />
+            <Text className="text-white text-xs  font-semibold">Update</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
-          <Icon name="cog" type="material-community" color="white" size={25} />
-          <Text className="text-white text-xs  font-semibold">Setting</Text>
-        </TouchableOpacity>
+          <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
+            <Icon
+              name="cog"
+              type="material-community"
+              color="white"
+              size={25}
+            />
+            <Text className="text-white text-xs  font-semibold">Setting</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
-          <Icon
-            name="logout"
-            type="material-community"
-            color="white"
-            size={25}
-          />
-          <Text className="text-white text-xs  font-semibold">Log out</Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
+            <Icon
+              name="logout"
+              type="material-community"
+              color="white"
+              size={25}
+            />
+            <Text className="text-white text-xs  font-semibold">Log out</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
