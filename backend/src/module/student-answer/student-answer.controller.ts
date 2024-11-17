18e81bfbf34a9b6c3884e812d1ada92193c4d363
Lmt310104 <lmtoan311@gmail.com>
2024-11-17 17:08:01 +0700

    add docs api

--- backend/src/module/student-answer/student-answer.controller.ts
+++ backend/src/module/student-answer/student-answer.controller.ts
@@ -1,17 +1,11 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
 } from '@nestjs/common';
 import { StudentAnswerService } from './student-answer.service';
-import { UpdateStudentAnswerDto } from './dto/update-student-answer.dto';
 import { DOCUMENTATION, END_POINTS } from 'src/utils/constants';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { StudentAnswerSubmit } from './dto/student-answer-submit.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/common/decorators/user.decorator';
 import { IUser } from 'src/common/guards/at.guard';
 import { ResponseObject } from 'src/utils/objects';
@@ -26,33 +20,11 @@ import { StudentAnswer } from './entities/student-answer.entity';
 export class StudentAnswerController {
   constructor(private readonly studentAnswerService: StudentAnswerService,   @InjectMapper() private readonly mapper: Mapper) {}
 
+  @ApiOperation({ summary: 'Submit student answer' })
   @Post(END_POINTS.STUDENT_ANSWER.SUBMIT_ANSWER)
   async submit(@Body() createStudentAnswerDto: CreateStudentAnswerDto[], @User() user: IUser, ) {
     const studentAnswers = await this.mapper.mapArray(createStudentAnswerDto, CreateStudentAnswerDto, StudentAnswer);
     const result = await this.studentAnswerService.submit(studentAnswers, user.userAwsId);
     return ResponseObject.create('Successfully',result);
-  }
-
-  @Get()
-  findAll() {
-    return this.studentAnswerService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.studentAnswerService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateStudentAnswerDto: UpdateStudentAnswerDto,
-  ) {
-    return this.studentAnswerService.update(+id, updateStudentAnswerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentAnswerService.remove(+id);
   }
 }
