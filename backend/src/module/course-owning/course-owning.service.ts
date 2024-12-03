79b7968d03a843897d5bab021b78c6b00bfa564b
Lmt310104 <lmtoan311@gmail.com>
2024-12-03 14:19:29 +0700

    feat: add new progress when student active course

--- backend/src/module/course-owning/course-owning.service.ts
+++ backend/src/module/course-owning/course-owning.service.ts
@@ -3,6 +3,9 @@ import { DataSource } from 'typeorm';
 import { CourseOwning } from './entities/course-owning.entity';
 import { Student } from '../user/entities/student.entity';
 import { Course } from '../course/entities/course.entity';
+import { CourseProgress } from './entities/course-progress.entity';
+import { LessonProgress } from './entities/lesson-progress.entity';
+import { SectionProgress } from './entities/section-progress.entity';
 
 @Injectable()
 export class CourseOwningService {
@@ -12,26 +15,46 @@ export class CourseOwningService {
       const student = await this.dataSource.getRepository(Student).findOne({
         where: { id: userAwsId },
       });
-      if (!student) {
-        throw new NotFoundException('Student not found');
-      }
       const course = await this.dataSource.getRepository(Course).findOne({
         where: { id: courseOwning.course.id },
       });
-      if (!course) {
-        throw new NotFoundException('Course not found');
-      }
       courseOwning.course = course;
       courseOwning.student = student;
-      const newCourseOwning = await this.dataSource
-        .getRepository(CourseOwning)
-        .save(courseOwning);
-      return newCourseOwning;
+      await this.dataSource.transaction(async (manager) => {
+        const newCourseOwning = await manager
+          .getRepository(CourseOwning)
+          .save(courseOwning);
+
+        const newCourseProgress = await manager
+          .getRepository(CourseProgress)
+          .save({
+            courseOwning: newCourseOwning,
+            course: course,
+          });
+        await Promise.all(
+          courseOwning.course.lessons.map(async (lesson) => {
+            const newLessonProgress = await manager
+              .getRepository(LessonProgress)
+              .save({
+                courseProgress: newCourseProgress,
+                lesson: lesson,
+              });
+            await Promise.all(
+              lesson.sections.map(async (section) => {
+                await manager.getRepository(SectionProgress).save({
+                  lessonProgress: newLessonProgress,
+                  section: section,
+                });
+              }),
+            );
+          }),
+        );
+        return newCourseOwning;
+      });
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
-
   findAll() {
     return `This action returns all courseOwning`;
   }
