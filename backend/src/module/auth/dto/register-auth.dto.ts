8ba988e16192ae1e951efcd97568301e0a01c98c
loingtan <0939899928za.com@gmail.com>
2024-12-15 21:23:12 +0700

    feat: remove fields

--- backend/src/module/auth/dto/register-auth.dto.ts
+++ backend/src/module/auth/dto/register-auth.dto.ts
@@ -42,39 +42,4 @@ export class RegisterAuthDto {
     description: 'Role of the user',
   })
   role: 'TEACHER' | 'STUDENT';
-
-  @AutoMap()
-  @IsOptional()
-  @ApiProperty({ description: 'First name of the user', default: 'John' })
-  firstName?: string; // Nullable field
-
-  @AutoMap()
-  @IsOptional()
-  @ApiProperty({ description: 'Last name of the user', default: 'Doe' })
-  lastName?: string; // Nullable field
-
-  @AutoMap()
-  @IsOptional()
-  @ApiProperty({
-    description: 'Phone number of the user',
-    default: '1234567890',
-  })
-  phone?: string; // Nullable field
-
-  @AutoMap()
-  @IsValidDOB()
-  @IsOptional()
-  @ApiProperty({ description: 'Date of birth', default: '1990-01-01' })
-  birthDate?: Date;
-  @ValidateIf((o) => o.role === 'TEACHER')
-  @IsNotEmpty({ message: 'Degree is required for teachers' })
-  @ApiPropertyOptional({
-    enum: ['BACHELOR', 'MASTER', 'DOCTOR', 'UNKNOWN'],
-    description: 'Degree of the teacher',
-  })
-  degree?: string;
-  @ValidateIf((o) => o.role === 'STUDENT')
-  @IsNotEmpty({ message: 'School name is required for students' })
-  @ApiPropertyOptional({ description: 'School name of the student' })
-  schoolName?: string;
 }
