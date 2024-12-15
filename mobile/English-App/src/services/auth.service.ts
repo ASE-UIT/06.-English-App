a1e5f6caa06d8ba03a7e9f4b49d7e189a8e53353
Tri Duong <duongthuantri159@gmail.com>
2024-12-15 15:09:53 +0700

    Feat: Create service for auth to call APIs

---
+++ mobile/English-App/src/services/auth.service.ts
@@ -0,0 +1,32 @@
+import http from "./Http";
+
+class AuthService {
+  baseURI: string;
+  constructor() {
+    this.baseURI = "auth/";
+  }
+  private getURI(uri: string) {
+    return `${this.baseURI}${uri}`;
+  }
+  async signUp(data: any) {
+    return await http.post(this.getURI("sign-up"), data);
+  }
+  async signIn(data: any) {
+    return await http.post(this.getURI("sign-in"), data);
+  }
+  async confirmSignUp(data: any) {
+    return await http.post(this.getURI("confirm-sign-up"), data);
+  }
+  async forgotPassword(data: any) {
+    return await http.post(this.getURI("forgot-password"), data);
+  }
+  async confirmForgotPassword(data: any) {
+    return await http.post(this.getURI("confirm-forgot-password"), data);
+  }
+  async signOut() {
+    return await http.post(this.getURI("sign-out"), {});
+  }
+}
+
+const authService = new AuthService();
+export default authService;
