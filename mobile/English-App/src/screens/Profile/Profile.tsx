0666d6c349404ffe482e1231ab04238e570a71d5
DPhngNam <22520908@gm.uit.edu.vn>
2024-12-19 00:33:45 +0700

    fix: rerender input profile

--- mobile/English-App/src/screens/Profile/Profile.tsx
+++ mobile/English-App/src/screens/Profile/Profile.tsx
@@ -21,24 +21,33 @@ import { LoginScreenNavigationProp } from "../../type";
 import * as SecureStore from "expo-secure-store";
 import { User } from "../../models";
 const Profile = () => {
-  const [profile, setProfile] = useState<User>();
-  const [isEditingName, setIsEditingName] = useState(false);
-  const nameInputRef = useRef<TextInput>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [avatar, setAvatar] = useState();
+  const [profile, setProfile] = useState({
+    id: "",
+    createDate: "",
+    updateDate: "",
+    role: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    birthDate: "",
+    avatarURL: "",
+    awsCognitoId: "",
+    additionalInfo: {
+      schoolName: "",
+    },
+  });
+  const handleInputChange = (field: string, value: string) => {
 
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const [date, setDate] = useState(new Date());
+    setProfile((prev) => ({ ...prev, [field]: value }));
+    console.log(profile);
+  };
   const [show, setShow] = useState(false);
   const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
     setShow(false);
 
     if (selectedDate) {
-      setDate(selectedDate);
+      setProfile((prev) => ({ ...prev, birthDate: selectedDate.toISOString() }));
     }
   };
 
@@ -57,15 +66,6 @@ const Profile = () => {
     fetchData();
   }, []);
 
-  const handleEditName = () => {
-    setIsEditingName(!isEditingName);
-    if (!isEditingName) {
-      setTimeout(() => {
-        nameInputRef.current?.focus();
-      }, 100);
-    }
-  };
-
   const handleUpdateProfile = async (profile: User) => {
     try {
       const result = await userService.updateUser(profile);
@@ -77,154 +77,121 @@ const Profile = () => {
     }
   };
 
-  if (!isLoaded) {
-    if (!isLoaded) {
-      <SafeAreaView>
-        <MainHeader />
-        <Text>Loading...</Text>
-      </SafeAreaView>;
-    }
-
-    return (
-      <SafeAreaView>
-        <MainHeader />
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View className="flex flex-col justify-center items-center">
-            <Avatar
-              source={{ uri: profile?.avatarURL }}
-              size="xlarge"
-              rounded
-              containerStyle={{ marginTop: 20 }}
-              onPress={() => console.log("Works!")}
-            />
-            <View className="w-[90%] justify-center items-center ">
-              <Text className="text-black ml-5 self-start text-base font-semibold leading-none">
-                BirthDate
-              </Text>
-              <TouchableOpacity onPress={() => setShow(true)} className="flex ">
-                <View className="bg-white rounded-[15px] shadow p-3">
-                  <Text className="text-black ml-[2px] text-base font-semibold leading-none">
-                    {date.toDateString()}
-                  </Text>
-                </View>
-                {show && (
-                  <DateTimePicker
-                    value={date} // Pass the current date value
-                    mode="date" // Use "time" for time picker or "datetime" for both
-                    display="default" // "default", "spinner", or "calendar" (iOS-specific options)
-                    onChange={onChange} // Callback when the date is changed
-                  />
-                )}
-              </TouchableOpacity>
-            </View>
-            <InputField
-              label="Phone Number"
-              value={phoneNumber}
-              onChangeText={setPhoneNumber}
-            />
-            <InputField label="Email" value={email} onChangeText={setEmail} />
-            <InputField
-              label="First Name"
-              value={firstName}
-              onChangeText={setFirstName}
-            />
-            <InputField
-              label="Last Name"
-              value={lastName}
-              onChangeText={setLastName}
-            />
-            <View
-              style={{ marginTop: 100 }}
-              className="flex flex-row justify-center items-center  gap-7"
+  return (
+    <SafeAreaView>
+      <MainHeader />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="flex flex-col justify-center items-center">
+          <Avatar
+            source={
+              profile?.avatarURL
+                ? { uri: profile.avatarURL }
+                : require("../../../assets/avatar.jpg")
+            }
+            size="xlarge"
+            rounded
+            containerStyle={{ marginTop: 20 }}
+            onPress={() => console.log("Works!")}
+          />
+          <View className="w-[90%] justify-center items-center ">
+            <Text className="text-black ml-5 self-start text-base font-semibold leading-none">
+              BirthDate
+            </Text>
+            <TouchableOpacity onPress={() => setShow(true)} className="flex ">
+              <View className="bg-white rounded-[15px] shadow p-3">
+                <Text className="text-black ml-[2px] text-base font-semibold leading-none">
+                  {new Date(profile.birthDate).toLocaleDateString("vi-VN", {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
+                </Text>
+              </View>
+              {show && (
+                <DateTimePicker
+                  value={new Date(profile.birthDate)}
+                  mode="date" // Use "time" for time picker or "datetime" for both
+                  display="default" // "default", "spinner", or "calendar" (iOS-specific options)
+                  onChange={onChange} // Callback when the date is changed
+                />
+              )}
+            </TouchableOpacity>
+          </View>
+          <InputField
+            label="First Name"
+            value={profile.firstName}
+            onChangeText={(value) => handleInputChange("firstname", value)}
+          />
+          <InputField
+            label="Last Name"
+            value={profile.lastName}
+            onChangeText={(value) => handleInputChange("lastname", value)}
+          />
+          <InputField label="Email" value={profile.email} onChangeText={(value) => handleInputChange("email", value)} />
+          <InputField
+            label="Phone Number"
+            value={profile.phone}
+            onChangeText={(value) => handleInputChange("phone", value)}
+          />
+          <View
+            style={{ marginTop: 100 }}
+            className="flex flex-row justify-center items-center  gap-7"
+          >
+            <TouchableOpacity
+              className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3"
+              onPress={() => {
+                //handleUpdateProfile({ ...profile, birthDate: new Date(profile.birthDate) });
+                console.log(profile);
+              }}
             >
-              <TouchableOpacity
-                className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3"
-                onPress={() => {
-                  if (profile) {
-                    if (phoneNumber !== profile.phone && phoneNumber !== "") {
-                      profile.phone = phoneNumber;
-                    }
-                    if (email !== profile.email && email !== "") {
-                      profile.email = email;
-                    }
-
-                    if (date !== profile.birthDate && date !== new Date()) {
-                      profile.birthDate = date;
-                    }
-
-                    if (firstName !== profile.firstName && firstName !== "") {
-                      profile.firstName = firstName;
-                    }
-
-                    if (lastName !== profile.lastName && lastName !== "") {
-                      profile.lastName = lastName;
-                    }
-
-                    handleUpdateProfile(profile);
-                  }
-                }}
-              >
-                <Icon
-                  name="checkbox-marked-outline"
-                  type="material-community"
-                  color="white"
-                  size={25}
-                />
-                <Text className="text-white text-xs  font-semibold">
-                  Update
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3">
-                <Icon
-                  name="cog"
-                  type="material-community"
-                  color="white"
-                  size={25}
-                />
-                <Text className="text-white text-xs  font-semibold">
-                  Setting
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3"
-                onPress={async () => {
-                  try {
-                    // because currently the signOut api is not working (timeout, return HTML instead of JSON), i will just remove the token from the storage and navigate to login screen
+              <Icon
+                name="checkbox-marked-outline"
+                type="material-community"
+                color="white"
+                size={25}
+              />
+              <Text className="text-white text-xs  font-semibold">
+                Update
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="flex-row justify-between items-center bg-secondary rounded-lg shadow p-3"
+              onPress={async () => {
+                try {
+                  // because currently the signOut api is not working (timeout, return HTML instead of JSON), i will just remove the token from the storage and navigate to login screen
+                  await SecureStore.deleteItemAsync("accessToken");
+                  await SecureStore.deleteItemAsync("refreshToken");
+                  loginNav.navigate("Login");
+                  const res = await authService.signOut();
+                  if (res.statusCode === 201) {
+                    console.log("Logged out");
                     await SecureStore.deleteItemAsync("accessToken");
                     await SecureStore.deleteItemAsync("refreshToken");
                     loginNav.navigate("Login");
-                    const res = await authService.signOut();
-                    if (res.statusCode === 201) {
-                      console.log("Logged out");
-                      await SecureStore.deleteItemAsync("accessToken");
-                      await SecureStore.deleteItemAsync("refreshToken");
-                      loginNav.navigate("Login");
-                    } else {
-                      console.error("Failed to log out: ", res.message);
-                    }
-                  } catch (err) {
-                    console.error("Failed to log out: ", err);
+                  } else {
+                    console.error("Failed to log out: ", res.message);
                   }
-                }}
-              >
-                <Icon
-                  name="logout"
-                  type="material-community"
-                  color="white"
-                  size={25}
-                />
-                <Text className="text-white text-xs  font-semibold">
-                  Log out
-                </Text>
-              </TouchableOpacity>
-            </View>
+                } catch (err) {
+                  console.error("Failed to log out: ", err);
+                }
+              }}
+            >
+              <Icon
+                name="logout"
+                type="material-community"
+                color="white"
+                size={25}
+              />
+              <Text className="text-white text-xs  font-semibold">
+                Log out
+              </Text>
+            </TouchableOpacity>
           </View>
-        </TouchableWithoutFeedback>
-      </SafeAreaView>
-    );
-  }
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  );
+
 };
 
 export default Profile;
